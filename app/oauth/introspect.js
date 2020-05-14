a37e8ec9df978038314bc75e1640146f53fd77aa
Graham Veal <grahamveal@gmail.com>
2020-05-14 09:06:29 +0100

    Fix typo for emailUserId
    Also make the case consistent in introspect.js

--- app/oauth/introspect.js
+++ app/oauth/introspect.js
@@ -1,4 +1,4 @@
-const introspect = (scope, username, emailUserID) => {
+const introspect = (scope, username, emailUserId) => {
   return function (req, res, next) {
     if (!scope) {
       return next(Error('Please provide scope'))
@@ -9,7 +9,7 @@ const introspect = (scope, username, emailUserID) => {
       active: true,
       exp: 2524608000,
       username,
-      email_user_id: emailUserID,
+      email_user_id: emailUserId,
     }
 
     return res.status(200).send(response)
